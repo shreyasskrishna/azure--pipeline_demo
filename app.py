e5a8770e9b18c14502eb6873b45bfa49fa14f703
SHREYASS  KRISHNA S A <shreyasskrishna09@gmail.com>
2025-12-09 14:38:16 +0530

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-if __name__ == "__main__":
-    now = datetime.now()
-    print("Current date and time:", now.strftime("%Y-%m-%d %H:%M:%S"))
-    print("Hello, Azure Pipeline!")
